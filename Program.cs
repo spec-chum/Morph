c1c18b4ee44c02d9248d0256cde70ca8c2f2489b
Spec-Chum <specchumster@gmail.com>
2024-11-02 23:18:34 +0000

    Add morphing between sphere and torus and code cleanup

--- Program.cs
+++ Program.cs
@@ -6,17 +6,23 @@ namespace Sphere;
 
 static class Program
 {
-    private const int Scale = 4;
-    private const int Width = 240;
-    private const int Height = 240;
-    private const int WindowWidth = Width * Scale;
-    private const int WindowHeight = Height * Scale;
-    
+    const int Scale = 4;
+    const int Width = 240;
+    const int Height = 240;
+    const int WindowWidth = Width * Scale;
+    const int WindowHeight = Height * Scale;
+
+    const float MorphSpeed = 0.005f;
+    const float HoldIncrement = 0.01f;
+    const float HoldDuration = 2f;
+
     static void Main()
     {
         InitWindow(WindowWidth, WindowHeight, "Sphere");
         SetTargetFPS(60);
-        
+
+        Vector2 centre = new(Width / 2, Height / 2);
+
         Image image = GenImageColor(Width, Height, Color.Black);
         Texture2D texture = LoadTextureFromImage(image);
 
@@ -26,49 +32,68 @@ static class Program
             pixels = new Span<Color>((Color*)image.Data, Width * Height);
         }
 
-        Vector3 centre = new(Width / 2, Height / 2, 0);
         Vector3[] spherePoints = GenerateSphere(100, 40, 20);
-        
+        Vector4 sphereColor = ColorNormalize(Color.DarkGreen);
+
+        Vector3[] torusPoints = GenerateTorus(70, 30, 40, 20);
+        Vector4 torusColor = ColorNormalize(Color.Maroon);
+
+        float morphTime = 0;
+        bool isMorphingToTorus = false;
+        float holdTime = 0f;
+
         while (!WindowShouldClose())
         {
             ImageClearBackground(ref image, Color.Black);
-            
-            float time = (float)GetTime();
 
-            Matrix4x4 rotX = Matrix4x4.CreateRotationX(time);
-            Matrix4x4 rotY = Matrix4x4.CreateRotationY(time);
-            Matrix4x4 rotZ = Matrix4x4.CreateRotationZ(time);
-            Matrix4x4 rotationMatrix = rotY * rotX * rotZ;
-            
-            foreach (Vector3 p in spherePoints)
+            morphTime += isMorphingToTorus ? MorphSpeed : -MorphSpeed;
+            morphTime = Math.Clamp(morphTime, 0, 1);
+
+            if (morphTime is 0 or 1)
             {
-                Vector3 point = Vector3.Transform(p, rotationMatrix);
-
-                // apply simple perspective correction
-                float perspective = 200f - point.Z;
-                point.X /= perspective;
-                point.Y /= perspective;
-
-                point = centre + point * centre;
-
-                // draw the points
-                ImageDrawPixel(ref image, (int)point.X, (int)point.Y, Color.DarkGreen);
+                holdTime += HoldIncrement;
+                if (holdTime >= HoldDuration)
+                {
+                    isMorphingToTorus = !isMorphingToTorus;
+                    morphTime = isMorphingToTorus ? 0 : 1;
+                    holdTime = 0;
+                }
             }
-            
+
+            float time = (float)GetTime();
+            Matrix4x4 rotationMatrix = Matrix4x4.CreateFromYawPitchRoll(time, time, time);
+
+            for (int i = 0; i < spherePoints.Length; i++)
+            {
+                // transform points with rotation matrix
+                Vector3 transformedPoint = Vector3.Transform(Vector3.Lerp(spherePoints[i], torusPoints[i], morphTime),
+                    rotationMatrix);
+
+                // apply simple perspective correction and map to screen space
+                float perspective = 200f - transformedPoint.Z;
+                Vector2 screenPoint = new Vector2(transformedPoint.X, transformedPoint.Y) / perspective * centre;
+
+                // centre screen point
+                screenPoint += centre;
+
+                Vector4 color = Vector4.Lerp(sphereColor, torusColor, morphTime);
+                ImageDrawPixelV(ref image, screenPoint, ColorFromNormalized(color));
+            }
+
             UpdateTexture<Color>(texture, pixels);
             BeginDrawing();
             DrawTextureEx(texture, Vector2.Zero, 0, Scale, Color.White);
             EndDrawing();
         }
     }
-    
-    // fill a Vector3 array with points for a sphere
+
     private static Vector3[] GenerateSphere(int radius, int numPointsHorizontal, int numPointsVertical)
     {
         Vector3[] points = new Vector3[numPointsHorizontal * numPointsVertical];
+
         float incrementHorizontal = MathF.Tau / numPointsHorizontal;
         float incrementVertical = MathF.PI / numPointsVertical;
-        
+
         int index = 0;
         for (int i = 0; i < numPointsVertical; i++)
         {
@@ -79,7 +104,32 @@ static class Program
                 float x = radius * MathF.Cos(theta) * MathF.Sin(phi);
                 float y = radius * MathF.Sin(theta) * MathF.Sin(phi);
                 float z = radius * MathF.Cos(phi);
-                
+
+                points[index++] = new Vector3(x, y, z);
+            }
+        }
+
+        return points;
+    }
+
+    private static Vector3[] GenerateTorus(float ringRadius, float tubeRadius, int numPointsHorizontal, int numPointsVertical)
+    {
+        Vector3[] points = new Vector3[numPointsHorizontal * numPointsVertical];
+
+        float incrementHorizontal = MathF.Tau / numPointsHorizontal;
+        float incrementVertical = MathF.Tau / numPointsVertical;
+
+        int index = 0;
+        for (int i = 0; i < numPointsVertical; i++)
+        {
+            float phi = i * incrementVertical;
+            for (int j = 0; j < numPointsHorizontal; j++)
+            {
+                float theta = j * incrementHorizontal;
+                float x = (ringRadius + tubeRadius * MathF.Cos(phi)) * MathF.Cos(theta);
+                float y = (ringRadius + tubeRadius * MathF.Cos(phi)) * MathF.Sin(theta);
+                float z = tubeRadius * MathF.Sin(phi);
+
                 points[index++] = new Vector3(x, y, z);
             }
         }
